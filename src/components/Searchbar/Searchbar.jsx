ba3ab899cbf16e0a76cf810b47a6542206165b5a
InnaKovrizhenko <114367918+InnaKovrizhenko@users.noreply.github.com>
2023-05-15 01:20:12 +0300

    finished

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,59 @@
+import React, { PureComponent } from 'react';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import PropTypes from 'prop-types';
+import {
+  SearchBar,
+  SearchForm,
+  SearchButton,
+  ButtonLabel,
+  SearchInput,
+} from './Searchbar.styled';
+
+export class Searchbar extends PureComponent {
+    static propTypes = {
+        onSubmit: PropTypes.func.isRequired,
+      };
+
+state = {
+    inputText: '',
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    if (this.state.inputText.trim() === '') {
+      toast.info('Please enter any request');
+      return;
+    }
+    this.props.onSubmit(this.state.inputText);
+    this.setState({ inputText: '' });
+  };
+
+  handleInput = event => {
+    this.setState({ inputText: event.currentTarget.value.toLowerCase() });
+  };
+
+  render() {
+    return (
+      <div>
+        <SearchBar>
+          <SearchForm onSubmit={this.handleSubmit}>
+            <SearchButton type="submit">
+              <ButtonLabel>Search</ButtonLabel>
+            </SearchButton>
+
+            <SearchInput
+              type="text"
+              autoComplete="off"
+              autoFocus
+              placeholder="Search images and photos"
+              onChange={this.handleInput}
+            />
+          </SearchForm>  
+        </SearchBar>
+      </div>
+    );
+  }
+}
+
+
